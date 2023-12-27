1dff7cd6381778e2b27f8b57b33d56ad88cf929b
zora-git <154859133+zora-git@users.noreply.github.com>
2023-12-27 16:50:52 +0900

    js weather api integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
 
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let description = document.querySelector("#weather-description");
+  description.innerHTML = response.data.condition.description;
+
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = response.data.wind.speed;
 }
 
 function searchCity(city) {
@@ -18,7 +26,14 @@ function seoulWeather(seoul) {
   let seoulTemperature = document.querySelector("#temperature-seoul");
   let temp = seoul.data.temperature.current;
   seoulTemperature.innerHTML = Math.round(temp);
+
+  let seoulHumidity = document.querySelector("#seoul-humidity");
+  seoulHumidity.innterHTML = seoul.data.temperature.humidity;
+
+  let seoulDescription = document.querySelector("#seoul-weather-description");
+  seoulDescription.innerHTML = seoul.data.condition.description;
 }
+
 function searchSeoul() {
   let apiKey = "6403o1d018cabaa3376f1c4c467ec4ft";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Seoul&key=${apiKey}&units=metric`;
