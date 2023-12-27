5ada69275ccfe6b9846d3418c21da67e5e02b905
zora-git <154859133+zora-git@users.noreply.github.com>
2023-12-27 15:16:23 +0900

    Added api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "6403o1d018cabaa3376f1c4c467ec4ft";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function seoulWeather(seoul) {
+  let seoulTemperature = document.querySelector("#temperature-seoul");
+  let temp = seoul.data.temperature.current;
+  seoulTemperature.innerHTML = Math.round(temp);
+}
+function searchSeoul() {
+  let apiKey = "6403o1d018cabaa3376f1c4c467ec4ft";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Seoul&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(seoulWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
+  searchSeoul();
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchSeoul();
