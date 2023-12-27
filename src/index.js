c0f9e3fd8c948fe558d4c1d044e010a7c5877c8c
zora-git <154859133+zora-git@users.noreply.github.com>
2023-12-27 17:31:35 +0900

    time corrections

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 
@@ -14,6 +13,38 @@ function refreshWeather(response) {
 
   let windSpeed = document.querySelector("#wind-speed");
   windSpeed.innerHTML = response.data.wind.speed;
+
+  let emojiElement = document.querySelector("#weather-emoji");
+  emojiElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-emoji"/>`;
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time);
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
